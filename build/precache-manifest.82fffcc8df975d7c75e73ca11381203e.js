4a631850b73f36ab23ae3f82e90adc003e65e980
Cesar Tzaj <cesar.tzaj@outlook.com>
2023-01-13 12:44:50 -0600

    se agrego otra version de  react

---
+++ build/precache-manifest.82fffcc8df975d7c75e73ca11381203e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2e0229a1410aff5f927",
+    "url": "/CesarTzaj/todos/static/css/main.2f47d9f3.chunk.css"
+  },
+  {
+    "revision": "f2e0229a1410aff5f927",
+    "url": "/CesarTzaj/todos/static/js/main.f2e0229a.chunk.js"
+  },
+  {
+    "revision": "513e1c128deccdb1119f",
+    "url": "/CesarTzaj/todos/static/js/1.513e1c12.chunk.js"
+  },
+  {
+    "revision": "8078cab20d79034d7540",
+    "url": "/CesarTzaj/todos/static/js/runtime~main.8078cab2.js"
+  },
+  {
+    "revision": "bc38581d6f4d8eadbefd06c7367fd5b9",
+    "url": "/CesarTzaj/todos/index.html"
+  }
+];
